f397fde4fa3cec9090c545c05e60867c76c12ee5
rezashakeri <rezashakeri2648@gmail.com>
2022-08-05 01:16:16 +0430

    feat: adding filetype library to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 'filetype>=1.1.0']
 
 test_requirements = ['pytest>=3', ]
 
