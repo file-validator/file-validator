2b3078f53640c2736d09ea0ab818096c6535213b
rezashakeri <rezashakeri2648@gmail.com>
2022-08-06 05:51:50 +0430

    refactor: adding comment to code and deleting unused imports

--- file_validator/django/validator.py
+++ file_validator/django/validator.py
@@ -2,13 +2,7 @@ from django.core.exceptions import ValidationError
 from filetype import (
     get_type,
     is_extension_supported,
-    match,
     guess,
-    is_audio,
-    is_video,
-    is_font,
-    is_image,
-    is_archive,
 )
 from django.core.files.uploadedfile import TemporaryUploadedFile
 from django.utils.deconstruct import deconstructible
@@ -17,6 +11,7 @@ from django.utils.deconstruct import deconstructible
 @deconstructible
 class FileValidator:
     """ """
+
     def __init__(self, *args):
         """
         :param args: You can choose different types and pass it as a string and be sure to separate the types with commas, example : FileValidator("mp3", "avi")
@@ -47,6 +42,10 @@ class FileValidator:
         file_path = TemporaryUploadedFile.temporary_file_path(file)
         result_validation = []
         current_file = guess(file_path)
+        """
+        Here, if we cannot detect the upload file type using
+        the filetype library, we will create a validation error.
+        """
         if current_file is not None:
             for file_extension, temporary_file in self.selected_types.items():
                 if (
@@ -61,7 +60,18 @@ class FileValidator:
                     result_validation.append(False)
         else:
             raise ValidationError("Please Upload a Valid File")
-
+        """
+        Here we compare the file with all the types we have determined
+        If the type and extension of the files do not match what we had
+        in the model, we will return the True value, otherwise, if both
+        the value and the type are equal to one of the types we specified,
+        it will return the False value, and if the result The validation of
+        each file is returned to True, so here we can understand that this
+        file does not match any of the types of files that we specified, and
+        using the all() function, we AND (&) the True values in result_validation
+        together. and show a validation error, otherwise, if only one returns False,
+        then the file has been successfully validated.
+        """
         file_is_not_valid = all(result_validation)
         if file_is_not_valid:
             raise ValidationError(
