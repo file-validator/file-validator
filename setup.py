139a4393b24f4f091ebfd63a8af1a070e644183c
rezashakeri <rezashakeri2648@gmail.com>
2022-08-08 04:27:17 +0430

    refactor: add version 0.0.5 to setup.py

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/rzashakeri/file_validator",
-    version="0.0.4",
+    version="0.0.5",
     zip_safe=False,
 )
