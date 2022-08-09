4b1075b3fa80e9e9f81547424d9eff3341443e7d
rezashakeri <rezashakeri2648@gmail.com>
2022-08-10 03:14:04 +0430

    test: adding test for pure python validator

--- tests/test_file_validator.py
+++ tests/test_file_validator.py
@@ -1,37 +1,55 @@
-#!/usr/bin/env python
-
-"""Tests for `file_validator` package."""
-
 import pytest
-
-from click.testing import CliRunner
-
 from file_validator import file_validator
-from file_validator import cli
+import os
+import magic
+from file_validator.file_validator.validator import (
+    get_mime_by_python_magic,
+    get_mime_by_filetype,
+    get_mime_by_mimetypes,
+    get_extension_by_filetype,
+    file_validation_by_filetype,
+)
+from filetype import guess, is_mime_supported, is_extension_supported
+from .fixtures import (
+    MP3_EXTENSION,
+    MP3_MIME,
+    JPEG_EXTENSION,
+    JPEG_MIME,
+    BAD_MIME,
+    jpeg_file,
+    mp3_file,
+    text_file
+)
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_get_mime_by_python_magic_return_correct_mime(jpeg_file):
+    file_mime = get_mime_by_python_magic(jpeg_file)
+    assert file_mime == JPEG_MIME
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_get_mime_by_filetype_return_correct_mime(jpeg_file):
+    file_mime = get_mime_by_filetype(jpeg_file)
+    assert file_mime == JPEG_MIME
 
 
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'file_validator.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+def test_get_mime_by_mimetypes_return_correct_mime(jpeg_file):
+    file_mime = get_mime_by_mimetypes(jpeg_file)
+    assert file_mime == JPEG_MIME
+
+
+def test_get_extension_by_filetype_return_correct_extension(jpeg_file):
+    file_extension = get_extension_by_filetype(jpeg_file)
+    assert file_extension == JPEG_EXTENSION
+
+
+def test_file_validation_by_filetype_library_when_file_is_valid(jpeg_file):
+    result_of_validation = file_validation_by_filetype(JPEG_MIME, file_path=jpeg_file)
+    assert result_of_validation is None
+
+
+def test_file_validation_by_filetype_library_when_file_is_not_valid(mp3_file):
+    try:
+        file_validation_by_filetype(JPEG_MIME, file_path=mp3_file)
+    except ValueError as error:
+        assert MP3_MIME in str(error)
+
