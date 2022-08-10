430d64e50622d9eaef89b24fed7959a53706c9f9
rezashakeri <rezashakeri2648@gmail.com>
2022-08-10 20:30:50 +0430

    test: full completion test of pure python validator (file validator)

--- tests/test_file_validator.py
+++ tests/test_file_validator.py
@@ -8,6 +8,9 @@ from file_validator.file_validator.validator import (
     get_mime_by_mimetypes,
     get_extension_by_filetype,
     file_validation_by_filetype,
+    file_validation_by_python_magic,
+    file_validation_by_mimetypes,
+    file_validation
 )
 from filetype import guess, is_mime_supported, is_extension_supported
 from .fixtures import (
@@ -18,38 +21,82 @@ from .fixtures import (
     BAD_MIME,
     jpeg_file,
     mp3_file,
-    text_file
+    text_file,
+    PNG_MIME,
 )
 
 
-def test_get_mime_by_python_magic_return_correct_mime(jpeg_file):
-    file_mime = get_mime_by_python_magic(jpeg_file)
-    assert file_mime == JPEG_MIME
+class TestFileValidationByPythonMagic:
+    def test_get_mime_by_python_magic_return_correct_mime(self, jpeg_file):
+        file_mime = get_mime_by_python_magic(jpeg_file)
+        assert file_mime == JPEG_MIME
+
+    def test_file_validation_by_python_magic_library_when_file_is_valid(
+        self, jpeg_file
+    ):
+        result_of_validation = file_validation_by_python_magic(
+            JPEG_MIME, file_path=jpeg_file
+        )
+        assert result_of_validation is None
+
+    def test_file_validation_by_python_magic_library_when_file_is_not_valid(
+        self, jpeg_file
+    ):
+        try:
+            file_validation_by_python_magic(PNG_MIME, file_path=jpeg_file)
+        except ValueError as error:
+            assert JPEG_MIME in str(error)
 
 
-def test_get_mime_by_filetype_return_correct_mime(jpeg_file):
-    file_mime = get_mime_by_filetype(jpeg_file)
-    assert file_mime == JPEG_MIME
+class TestFileValidationByFileType:
+    def test_get_mime_by_filetype_return_correct_mime(self, jpeg_file):
+        file_mime = get_mime_by_filetype(jpeg_file)
+        assert file_mime == JPEG_MIME
+
+    def test_get_extension_by_filetype_return_correct_extension(self, jpeg_file):
+        file_extension = get_extension_by_filetype(jpeg_file)
+        assert file_extension == JPEG_EXTENSION
+
+    def test_file_validation_by_filetype_library_when_file_is_valid(self, jpeg_file):
+        result_of_validation = file_validation_by_filetype(
+            JPEG_MIME, file_path=jpeg_file
+        )
+        assert result_of_validation is None
+
+    def test_file_validation_by_filetype_library_when_file_is_not_valid(self, mp3_file):
+        try:
+            file_validation_by_filetype(JPEG_MIME, file_path=mp3_file)
+        except ValueError as error:
+            assert MP3_MIME in str(error)
 
 
-def test_get_mime_by_mimetypes_return_correct_mime(jpeg_file):
-    file_mime = get_mime_by_mimetypes(jpeg_file)
-    assert file_mime == JPEG_MIME
+class TestFileValidationByMimeTypes:
+    def test_get_mime_by_mimetypes_return_correct_mime(self, jpeg_file):
+        file_mime = get_mime_by_mimetypes(jpeg_file)
+        assert file_mime == JPEG_MIME
+
+    def test_file_validation_by_mimetypes_library_when_file_is_valid(self, jpeg_file):
+        result_of_validation = file_validation_by_mimetypes(
+            JPEG_MIME, file_path=jpeg_file
+        )
+        assert result_of_validation is None
+
+    def test_file_validation_by_mimetypes_library_when_file_is_not_valid(
+        self, jpeg_file
+    ):
+        try:
+            file_validation_by_mimetypes(PNG_MIME, file_path=jpeg_file)
+        except ValueError as error:
+            assert JPEG_MIME in str(error)
 
 
-def test_get_extension_by_filetype_return_correct_extension(jpeg_file):
-    file_extension = get_extension_by_filetype(jpeg_file)
-    assert file_extension == JPEG_EXTENSION
-
-
-def test_file_validation_by_filetype_library_when_file_is_valid(jpeg_file):
-    result_of_validation = file_validation_by_filetype(JPEG_MIME, file_path=jpeg_file)
+def test_file_validation_when_file_is_valid(jpeg_file):
+    result_of_validation = file_validation(JPEG_MIME, file_path=jpeg_file)
     assert result_of_validation is None
 
 
-def test_file_validation_by_filetype_library_when_file_is_not_valid(mp3_file):
+def test_file_validation_when_file_is_not_valid(jpeg_file):
     try:
-        file_validation_by_filetype(JPEG_MIME, file_path=mp3_file)
+        file_validation(PNG_MIME, file_path=jpeg_file)
     except ValueError as error:
-        assert MP3_MIME in str(error)
-
+        assert JPEG_MIME in str(error)
