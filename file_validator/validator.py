322b0eaf02cb2d6b7a0735ee3efe6b27a16f0210
rezashakeri <rezashakeri2648@gmail.com>
2022-08-11 02:46:22 +0430

    refactor: deleting duplicate functions

--- file_validator/validator.py
+++ file_validator/validator.py
@@ -1,98 +1,55 @@
-from filetype import guess, is_mime_supported, is_extension_supported
-import magic
-from mimetypes import guess_type, guess_extension
+from filetype import guess
+from mimetypes import guess_type
 from termcolor import colored
+import magic
 
-from file_validator.tests.fixtures import BAD_MIME, text_file, PNG_MIME, jpeg_file
 
-
-def get_mime_by_python_magic(file_path):
+def file_validator_by_python_magic(*args, file_path):
     """
     :type file_path: string
-    :param file_path: The path of the file you want to get MIME use python magic library
-    :return: return MIME of file you gave it
+    :param file_path: The path to the file you want to validate
+    :param args: The mime of the files you want to validate based on them, example: image/png, video/mp4
     """
     file_mime = magic.from_buffer(open(file_path, "rb").read(2048), mime=True)
-    return file_mime
+    if file_mime not in list(args):
+        error_message = f"""
+        {file_mime} is not valid
+        """
+        raise ValueError(colored(error_message, "red"))
 
 
-def get_mime_by_filetype(file_path):
+def file_validator_by_mimetypes(*args, file_path):
     """
     :type file_path: string
-    :param file_path: The path of the file you want to get MIME use filetype library
-    :return: return MIME of file you gave it
-    """
-    file_mime = guess(file_path).MIME
-    return file_mime
-
-
-def get_mime_by_mimetypes(file_path):
-    """
-    :type file_path: string
-    :param file_path: The path of the file you want to get MIME use mim library (native library)
-    :return: return extension of file you gave it
+    :param file_path: The path to the file you want to validate
+    :param args: The mime of the files you want to validate based on them, example: image/png, video/mp4
     """
     file_mime = guess_type(file_path)[0]
-    return file_mime
+    if file_mime not in list(args):
+        error_message = f"""
+        {file_mime} is not valid
+        """
+        raise ValueError(colored(error_message, "red"))
 
 
-def get_extension_by_filetype(file_path):
-    """
-    :type file_path: string
-    :param file_path: The path of the file you want to get the extension use filetype library
-    :return: return extension of file you gave it
-    """
-    file_extension = guess(file_path).EXTENSION
-    return file_extension
-
-
-def file_validation_by_filetype(*args, file_path):
+def file_validator_by_filetype(*args, file_path):
     """
     :type file_path: string
     :param file_path: The path to the file you want to validate
     :param args: The mime of the files you want to validate based on them, example : mp3, png, mp4 ...
     """
-    file_mime = get_mime_by_filetype(file_path)
+    file_mime = guess(file_path).MIME
     if file_mime not in list(args):
         error_message = f"{file_mime} is not valid"
         raise ValueError(colored(error_message, "red"))
 
 
-def file_validation_by_python_magic(*args, file_path):
+def file_validator(*args, file_path):
     """
     :type file_path: string
     :param file_path: The path to the file you want to validate
     :param args: The mime of the files you want to validate based on them, example: image/png, video/mp4
     """
-    file_mime = get_mime_by_python_magic(file_path)
-    if file_mime not in list(args):
-        error_message = f"""
-        {file_mime} is not valid
-        """
-        raise ValueError(colored(error_message, "red"))
-
-
-def file_validation_by_mimetypes(*args, file_path):
-    """
-    :type file_path: string
-    :param file_path: The path to the file you want to validate
-    :param args: The mime of the files you want to validate based on them, example: image/png, video/mp4
-    """
-    file_mime = get_mime_by_mimetypes(file_path)
-    if file_mime not in list(args):
-        error_message = f"""
-        {file_mime} is not valid
-        """
-        raise ValueError(colored(error_message, "red"))
-
-
-def file_validation(*args, file_path):
-    """
-    :type file_path: string
-    :param file_path: The path to the file you want to validate
-    :param args: The mime of the files you want to validate based on them, example: image/png, video/mp4
-    """
-    file_validation_by_filetype(*args, file_path=file_path)
-    file_validation_by_python_magic(*args, file_path=file_path)
-    file_validation_by_mimetypes(*args, file_path=file_path)
-
+    file_validator_by_filetype(*args, file_path=file_path)
+    file_validator_by_mimetypes(*args, file_path=file_path)
+    file_validator_by_python_magic(*args, file_path=file_path)
