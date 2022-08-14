6014a2a0a0047485844175e18db8fb5b0a80d4fc
rezashakeri <rezashakeri2648@gmail.com>
2022-08-14 22:25:16 +0430

    docs: update custom error message documentation

--- file_validator/constants.py
+++ file_validator/constants.py
@@ -15,17 +15,13 @@ def error_message(
     """
     :param file: Returns the name of the file to be validated
     :param mimes: It returns the mimes on which the file is to be validated
-    :param message: The error message to be shown to the user when the file is not valid,
-    You can customize this message through the Django settings (settings.py), to display
-    the file name in the error message, just put the string {file} in the error message
-    you want, or to display the mimes that the file is based on To be validated,
-    just put {mimes} in your error message
+    :param message: The error message to be shown to the user when the file is not valid, You can customize this message through the Django settings (settings.py), to display the file name in the error message, just put the string {file} in the error message you want, or to display the mimes that the file is based on To be validated, just put {mimes} in your error message
     :return: In case of customization, it will return your error message, and otherwise it will return "{file} is not valid" message.
     """
     file_mimes = ""
     for mime in mimes:
         file_mimes += str(mime)
-        file_mimes += ', '
+        file_mimes += ", "
         if mime == mimes[-1]:
             file_mimes += str(mime)
     return message.format(file=file, mimes=file_mimes)
