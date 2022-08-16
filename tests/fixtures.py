4242abd074811a53a13053f0087a6e59a4b37b19
rezashakeri <rezashakeri2648@gmail.com>
2022-08-16 10:30:24 +0430

    refactor: 1. add docstring to class 2. add docstring to function 3. change fixtures of test

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,34 +1,29 @@
+"""
+This file is related to fixtures and constants required for tests
+"""
 import os
-import pytest
 
 BAD_MIME = "mime/bad"
 JPEG_MIME = "image/jpeg"
 MP3_MIME = "audio/mpeg"
 PNG_MIME = "image/png"
+
 JPEG_EXTENSION = "jpg"
 MP3_EXTENSION = "mp3"
 PNG_EXTENSION = "png"
 
 
 def get_test_file(file_name):
+    """
+    :param file_name: The name of the test file
+    :return: It should return the path of the test file that is in the project
+    """
     test_directory = os.path.dirname(os.path.realpath(__file__))
     test_files_directory = os.path.join(test_directory, "test_files", f"{file_name}")
     return test_files_directory
 
 
-@pytest.fixture
-def jpeg_file():
-    file = get_test_file(file_name="test.jpg")
-    return file
-
-
-@pytest.fixture
-def mp3_file():
-    file = get_test_file(file_name="test.mp3")
-    return file
-
-
-@pytest.fixture
-def text_file():
-    file = get_test_file(file_name="test.txt")
-    return file
+JPEG_FILE = get_test_file("test.jpg")
+MP3_FILE = get_test_file("test.mp3")
+PNG_FILE = get_test_file("test.png")
+BAD_FILE = get_test_file("test.bad")
--- tests/test_file_validator.py
+++ tests/test_file_validator.py
@@ -1,58 +1,90 @@
-from .fixtures import (
-    MP3_MIME,
-    JPEG_MIME,
-    PNG_MIME,
-    jpeg_file,
-    mp3_file
-)
+"""
+This module is related to tests
+"""
 from file_validator.file_validator.validator import (
     file_validator_by_python_magic,
     file_validator_by_mimetypes,
     file_validator_by_filetype,
-    file_validator
+    file_validator,
 )
+from .fixtures import MP3_MIME, JPEG_MIME, PNG_MIME, JPEG_FILE, MP3_FILE
 
 
 class TestFileValidatorByPythonMagic:
-    def test_file_validator_by_python_magic_library_when_file_is_valid(self, jpeg_file):
-        result_of_validation = file_validator_by_python_magic(
-            JPEG_MIME, file_path=jpeg_file
-        )
-        assert result_of_validation is None
+    """
+    These tests are for file validators that are made using the python-magic library
+    """
+
+    def test_file_validator_by_python_magic_library_when_file_is_valid(
+        self, jpeg=JPEG_FILE
+    ):
+        """
+        :param jpeg: It is a fixture for jpeg files
+        :return: The result we expect to return is None, which means that everything is OK
+        """
+        assert file_validator_by_python_magic(JPEG_MIME, file_path=jpeg) is None
 
     def test_file_validator_by_python_magic_library_when_file_is_not_valid(
-        self, jpeg_file
+        self, jpeg=JPEG_FILE
     ):
+        """
+        :param jpeg: It is a fixture for jpeg files
+        :return: The result we expect is a return value error, which means that the file is invalid
+        """
         try:
-            file_validator_by_python_magic(PNG_MIME, file_path=jpeg_file)
+            file_validator_by_python_magic(PNG_MIME, file_path=jpeg)
         except ValueError as error:
             assert JPEG_MIME in str(error)
 
 
 class TestFileValidatorByMimeTypes:
-    def test_file_validator_by_mimetypes_library_when_file_is_valid(self, jpeg_file):
-        result_of_validation = file_validator_by_mimetypes(
-            JPEG_MIME, file_path=jpeg_file
-        )
-        assert result_of_validation is None
+    """
+    These tests are for file validators that are made using the mimetypes library
+    """
+
+    def test_file_validator_by_mimetypes_library_when_file_is_valid(
+        self, jpeg=JPEG_FILE
+    ):
+        """
+        :param jpeg: It is a fixture for jpeg files
+        :return: The result we expect to return is None, which means that everything is OK
+        """
+        assert file_validator_by_mimetypes(JPEG_MIME, file_path=jpeg) is None
 
     def test_file_validator_by_mimetypes_library_when_file_is_not_valid(
-        self, jpeg_file
+        self, jpeg=JPEG_FILE
     ):
+        """
+        :param jpeg: It is a fixture for jpeg files
+        :return: The result we expect is a return value error, which means that the file is invalid
+        """
         try:
-            file_validator_by_mimetypes(PNG_MIME, file_path=jpeg_file)
+            file_validator_by_mimetypes(PNG_MIME, file_path=jpeg)
         except ValueError as error:
             assert JPEG_MIME in str(error)
 
 
 class TestFileValidatorByFileType:
-    def test_file_validator_by_filetype_library_when_file_is_valid(self, jpeg_file):
-        result_of_validation = file_validator_by_filetype(
-            JPEG_MIME, file_path=jpeg_file
-        )
-        assert result_of_validation is None
+    """
+    These tests are for file validators that are made using the filetype library
+    """
 
-    def test_file_validator_by_filetype_library_when_file_is_not_valid(self, mp3_file):
+    def test_file_validator_by_filetype_library_when_file_is_valid(
+        self, jpeg=JPEG_FILE
+    ):
+        """
+        :param jpeg: It is a fixture for jpeg files
+        :return: The result we expect to return is None, which means that everything is OK
+        """
+        assert file_validator_by_filetype(JPEG_MIME, file_path=jpeg) is None
+
+    def test_file_validator_by_filetype_library_when_file_is_not_valid(
+        self, mp3_file=MP3_FILE
+    ):
+        """
+        :param mp3_file: It is a fixture for mp3 files
+        :return: The result we expect is a return value error, which means that the file is invalid
+        """
         try:
             file_validator_by_filetype(JPEG_MIME, file_path=mp3_file)
         except ValueError as error:
@@ -60,13 +92,23 @@ class TestFileValidatorByFileType:
 
 
 class TestFileValidator:
+    """
+    These tests are for file validators that are made using the filetype library
+    """
 
-    def test_file_validator_when_file_is_valid(self, jpeg_file):
-        result_of_validation = file_validator(JPEG_MIME, file_path=jpeg_file)
-        assert result_of_validation is None
+    def test_file_validator_when_file_is_valid(self, jpeg=JPEG_FILE):
+        """
+        :param jpeg: It is a fixture for jpeg files
+        :return: The result we expect to return is None, which means that everything is OK
+        """
+        assert file_validator(JPEG_MIME, file_path=jpeg) is None
 
-    def test_file_validation_when_file_is_not_valid(self, jpeg_file):
+    def test_file_validation_when_file_is_not_valid(self, jpeg=JPEG_FILE):
+        """
+        :param jpeg: It is a fixture for jpeg files
+        :return: The result we expect is a return value error, which means that the file is invalid
+        """
         try:
-            file_validator(PNG_MIME, file_path=jpeg_file)
+            file_validator(PNG_MIME, file_path=jpeg)
         except ValueError as error:
             assert JPEG_MIME in str(error)
