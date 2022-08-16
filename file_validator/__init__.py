89855d248bc1e705a313d2ca4ebd168482bcfed6
rezashakeri <rezashakeri2648@gmail.com>
2022-08-16 13:40:07 +0430

    refactor: update __init__.py

--- file_validator/__init__.py
+++ file_validator/__init__.py
@@ -4,9 +4,9 @@ from django.core.files.uploadedfile import TemporaryUploadedFile
 from django.utils.deconstruct import deconstructible
 from filetype import get_type, guess, is_mime_supported
 from termcolor import colored
-from file_validator.file_validator import exceptions
-from file_validator.file_validator import validator
-from file_validator.file_validator import django
+from . import exceptions
+from . import validator
+from . import django
 
 __author__ = """Reza Shakeri"""
 __email__ = 'rzashakeri@outlook.com'
