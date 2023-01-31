c361a691edab23b8dd1524b8362940c71ea091d9
Reza Shakeri <rzashakeri@gmail.com>
2023-01-31 18:24:55 +0330

    feat: add django settings

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,7 @@
 """Unit test package for file_validator."""
-from file_validator.file_validator.validators import (
-    file_validator_by_python_magic,
-    file_validator_by_mimetypes,
-    file_validator_by_filetype,
-    file_validator,
-)
-from .fixtures import MP3_MIME, JPEG_MIME, PNG_MIME, JPEG_FILE, MP3_FILE
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.project.project.settings')
+
+django.setup()
