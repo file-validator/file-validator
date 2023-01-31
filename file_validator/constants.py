02794fbfb55c72309bed5541f4ba02b431aca027
Reza Shakeri <rzashakeri@gmail.com>
2023-01-31 18:42:46 +0330

    feat: adding new constants

--- file_validator/constants.py
+++ file_validator/constants.py
@@ -1,22 +1,37 @@
 """
 In this module, the constants used at the library level are defined
 """
-PYTHON_MAGIC = "python_magic"
-PURE_MAGIC = "pure_magic"
-FILETYPE = "filetype"
-MIMETYPES = "mimetypes"
-DEFAULT = "default"
-ALL_SUPPORTED_LIBRARIES = [PYTHON_MAGIC, PURE_MAGIC, FILETYPE, MIMETYPES]
-SELECTING_ALL_SUPPORTED_LIBRARIES = "all"
-ZERO = 0
-MIMES_EMPTY = "The args value is empty, please pass the value (file MIME)."
-MIME_NOT_VALID = "mime is not a valid"
-MIME_NOT_VALID_WITH_MIME_NAME = "{file_mime} is not valid"
-FILE_IS_NOT_VALID = "{file} is not valid"
-FILE_SIZE_IS_NOT_VALID = (
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PYTHON_MAGIC: str = "python_magic"
+PURE_MAGIC: str = "pure_magic"
+FILETYPE: str = "filetype"
+MIMETYPES: str = "mimetypes"
+DEFAULT: str = "default"
+ALL: str = "all"
+ALL_SUPPORTED_LIBRARIES: list = [
+    PYTHON_MAGIC,
+    PURE_MAGIC,
+    FILETYPE,
+    MIMETYPES,
+    DEFAULT,
+    ALL,
+]
+SELECTING_ALL_SUPPORTED_LIBRARIES: str = "all"
+ZERO: int = 0
+MIMES_EMPTY: str = "The args value is empty, please pass the value (file MIME)."
+MIME_NOT_VALID: str = "mime is not a valid"
+MIME_NOT_VALID_WITH_MIME_NAME: str = "{file_mime} is not valid"
+FILE_IS_NOT_VALID: str = "{file} is not valid"
+FILE_SIZE_IS_NOT_VALID: str = (
     "{file_size} is not valid size, you can upload files up to {max_file_size}."
 )
-MIMES_IS_EQUAL = "The mimes ({mimes}) are similar, please use different mimes"
-LIBRARY_IS_NOT_SUPPORTED = (
+MIMES_IS_EQUAL: str = "The mimes ({mimes}) are similar, please use different mimes"
+LIBRARY_IS_NOT_SUPPORTED: str = (
     "{library} is not supported, you can choice library from this list : {libraries} "
 )
+PATH_MAGIC_FILE: str = os.environ["PATH_MAGIC_FILE"]
+DEFAULT_ERROR_MESSAGE: str = "{file} is not valid"
