9b08117eff8d5017a6fb09820a401f10caec4848
Reza Shakeri <rzashakeri@gmail.com>
2023-02-05 11:30:48 +0330

    refactor: removing unused imports

--- file_validator/__init__.py
+++ file_validator/__init__.py
@@ -1,10 +1,4 @@
 """Top-level package for File Validator."""
-from django.core.exceptions import ValidationError
-from django.core.files.uploadedfile import TemporaryUploadedFile
-from django.utils.deconstruct import deconstructible
-from django.template.defaultfilters import filesizeformat
-from filetype import get_type, guess, is_mime_supported
-from termcolor import colored
 import file_validator.exceptions
 import file_validator.validators
 import file_validator.constants
