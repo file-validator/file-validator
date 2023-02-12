5d0f3d1e54711af3b95b1ecf937c2a2855519638
Reza Shakeri <rzashakeri@gmail.com>
2023-02-12 14:15:50 +0330

    ✨ feat: add ValidatedFileFiled for forms

---
+++ file_validator/forms.py
@@ -0,0 +1,23 @@
+"""
+this file provides forms for django
+"""
+from django import forms
+
+from file_validator.widgets import FileInputWidget
+
+
+class ValidatedFileFiled(forms.FileField):
+    """
+    validated file filed for django
+    """
+    widget = FileInputWidget
+
+    def __init__(self, *, accept=None, **kwargs):
+        self.accept = accept
+
+        super().__init__(**kwargs)
+
+    def widget_attrs(self, widget):
+        if self.accept:
+            return dict(accept=self.accept)
+        return {}
