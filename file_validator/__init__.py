736e3580724a31f5be631649dd4f23f4e7aa6ed0
Reza Shakeri <rzashakeri@gmail.com>
2023-02-14 04:08:52 +0330

    ✨ feat: add version 0.2.3

--- file_validator/__init__.py
+++ file_validator/__init__.py
@@ -3,7 +3,10 @@ import file_validator.exceptions
 import file_validator.validators
 import file_validator.constants
 import file_validator.models
+import file_validator.forms
+import file_validator.widgets
+import file_validator.utils
 
 __author__ = """Reza Shakeri"""
 __email__ = "rzashakeri@outlook.com"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
