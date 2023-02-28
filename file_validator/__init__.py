a0b6c238f88807c9e13714e6c9925ef9228fe76d
Reza Shakeri <rzashakeri@gmail.com>
2023-02-28 16:16:15 +0330

    ♻️ refactor: removed unused imports

--- file_validator/__init__.py
+++ file_validator/__init__.py
@@ -1,11 +1,4 @@
 """Top-level package for File Validator."""
-import file_validator.exceptions
-import file_validator.validators
-import file_validator.constants
-import file_validator.models
-import file_validator.forms
-import file_validator.widgets
-import file_validator.utils
 
 __author__ = """Reza Shakeri"""
 __email__ = "rzashakeri@outlook.com"
