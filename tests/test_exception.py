0aeffa2d4846ac755a72c1958a9ca14583e77f43
Reza Shakeri <rzashakeri@gmail.com>
2023-03-10 13:51:26 +0330

    ✅ test: added test_exception.py

---
+++ tests/test_exception.py
@@ -0,0 +1,22 @@
+"""Tests for exception."""
+from file_validator.exceptions import error_message
+
+from tests.fixtures import EXPECTED_MESSAGE, PNG_OBJECT, TEMPLATE_EXPECTED_MESSAGE
+
+
+class TestException:
+    """Tests for exceptions."""
+
+    @staticmethod
+    def test_error_message_function_return_correct_message():
+        """We test whether this error message function returns the expected
+        message or not."""
+        message = error_message(
+            message=TEMPLATE_EXPECTED_MESSAGE,
+            file_name=PNG_OBJECT["name"],
+            file_size="20 MB",
+            mimes=["image/png", "audio/mpeg"],
+            max_file_size="10 MB",
+            current_file_mime=PNG_OBJECT["mime"],
+        )
+        assert EXPECTED_MESSAGE in message
