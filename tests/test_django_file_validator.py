46b0519ba55cde2f360ce2da23ab32e4f183990a
Reza shakeri <rzashakeri@gmail.com>
2023-09-08 23:57:59 +0330

    ✅ test: added test_django_file_validator_when_file_mime_guessed_by_django_is_none

--- tests/test_django_file_validator.py
+++ tests/test_django_file_validator.py
@@ -10,7 +10,6 @@ from file_validator.exceptions import (
     TypeNotSupportedException,
 )
 from file_validator.models import DjangoFileValidator
-
 from tests.fixtures import (
     BAD_OBJECT,
     get_tmp_file,
@@ -242,4 +241,18 @@ class TestDjangoFileValidator:
             ),
         )
 
+        new_instance.full_clean()
+
+    @staticmethod
+    def test_django_file_validator_when_file_mime_guessed_by_django_is_none():
+        """test django_file_validator when file_mime_guessed_by_django is none"""
+        new_instance = ModelWithDjangoFileValidator(
+            test_file=get_tmp_file(
+                file_name=PNG_OBJECT[NAME],
+                file_path=PNG_FILE,
+                file_mime_type=PNG_OBJECT[MIME],
+            ),
+        )
+
+        del new_instance.test_file.file.content_type
         new_instance.full_clean()
